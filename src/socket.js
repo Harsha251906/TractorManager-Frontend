d73066d7d2fb0712143d4ad1a4b95b7ccaecf356
Harsha <harsha252303@gmail.com>
2026-07-23 13:57:35 +0530

    Fix socket for production

--- src/socket.js
+++ src/socket.js
@@ -1,11 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
-
+const socket = io("https://tractormanager-backend.onrender.com", {
     autoConnect: false,
-
     transports: ["websocket"]
-
 });
 
 export default socket;
